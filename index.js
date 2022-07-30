9ae067f02de0df3636486d830cef2c76dae55542
Nagendra121 <nagendra.singh.she@gmail.com>
2022-07-30 14:02:07 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,13 @@
+// typing animation script
+var typed = new Typed(".typing",{
+    strings :["Web Developer","Front-End Developer","Designer","FreeLancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2",{
+    strings :[" Designer"],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true
+});
